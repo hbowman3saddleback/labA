eaa398b7d8881ea92ad44e200aa121770d609e07
hbowman3saddleback <148819777+hbowman3saddleback@users.noreply.github.com>
2024-04-04 04:23:43 +0000

    new

---
+++ problem1.cpp
@@ -0,0 +1,7 @@
+//Write a program that asks the user to enter a sentence and then uses a while loop to capitalize the first letter of each word in the sentence.
+//Display the modified sentence.
+//Assume all the words are separated by a space characters from each other and there is no punctuation character in the string.
+#include <iostream>
+#include <string>
+using namespace std;
+
